fbcaf646c4d3f0cfcbac91076d8ac31f979bbff6
Justin Stewart <justinstewart.la@gmail.com>
2022-06-17 11:31:36 -0400

    added s3 data feed

--- ef_artsy_scraper/settings.py
+++ ef_artsy_scraper/settings.py
@@ -1,7 +1,25 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+# Main Scrapy Configs
 BOT_NAME = 'artsy'
 SPIDER_MODULES = ['ef_artsy_scraper.spiders']
 NEWSPIDER_MODULE = 'ef_artsy_scraper.spiders'
 CONCURRENT_REQUESTS = 64
+
+## AWS Configs
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
+
+# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+## S3 Filepath and Export Feed
+FEED_URI = 's3://euclidsfund-data-pipeline/data_acquisition/artsy/raw/%(name)s/%(time)s.json'
+FEED_FORMAT = 'json'
+
 
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
